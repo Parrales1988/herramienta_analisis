8c4038cacbbcfcfb0724693d8c30a7de4621e538
Parrales1988 <jeferson.parrales@casagrande.edu.ec>
2025-01-03 22:12:43 -0500

    Update HDADDYMDR_V1.1.py

--- HDADDYMDR_V1.1.py
+++ HDADDYMDR_V1.1.py
@@ -55,11 +55,15 @@ def cargar_dataset_kaggle():
                 api.authenticate()
 
                 st.success("Ingrese exitosamente.")
+
+                # Descarga el dataset
+                kaggle.api.dataset_download_files('/jackdaoud/marketing-data', path='.', unzip=True)
+        
                 # Listar datasets de Kaggle
-                datasets = api.dataset_list(page=1)  # Ajusta el número de página según tus necesidades
-                dataset_titles = [dataset.title for dataset in datasets]
-                st.write("Lista de datasets disponibles en Kaggle:")
-                st.write(dataset_titles)
+                #datasets = api.dataset_list(page=1)  # Ajusta el número de página según tus necesidades
+                #dataset_titles = [dataset.title for dataset in datasets]
+                #st.write("Lista de datasets disponibles en Kaggle:")
+                #st.write(dataset_titles)
 
                 # Verificar las credenciales
                 # user = api.get_user()
